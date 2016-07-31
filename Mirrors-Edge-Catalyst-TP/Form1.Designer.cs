bc24494d4eec33d358722647f977a56ad83dc122
captainanderz <anders.lindhardt999@gmail.com>
2016-07-31 18:21:25 +0200

    Updated design and added button functions

--- Mirrors-Edge-Catalyst-TP/Form1.Designer.cs
+++ Mirrors-Edge-Catalyst-TP/Form1.Designer.cs
@@ -31,10 +31,20 @@
             this.components = new System.ComponentModel.Container();
             this.CheckGameStatus = new System.Windows.Forms.Timer(this.components);
             this.Menu = new NSTheme();
+            this.nsGroupBox1 = new NSGroupBox();
+            this.nsLabel2 = new NSLabel();
+            this.TelePosition = new NSTextBox();
+            this.nsLabel1 = new NSLabel();
+            this.SavePosition = new NSTextBox();
+            this.GroupBox = new NSGroupBox();
+            this.TeleportButton = new NSButton();
+            this.SaveButton = new NSButton();
             this.nsControlButton1 = new NSControlButton();
             this.GameStatus = new System.Windows.Forms.Label();
             this.GameName = new System.Windows.Forms.Label();
             this.Menu.SuspendLayout();
+            this.nsGroupBox1.SuspendLayout();
+            this.GroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // CheckGameStatus
@@ -49,6 +59,8 @@
             this.Menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.Menu.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Menu.Colors = new Bloom[0];
+            this.Menu.Controls.Add(this.nsGroupBox1);
+            this.Menu.Controls.Add(this.GroupBox);
             this.Menu.Controls.Add(this.nsControlButton1);
             this.Menu.Controls.Add(this.GameStatus);
             this.Menu.Controls.Add(this.GameName);
@@ -68,6 +80,109 @@
             this.Menu.Text = "Teleporter - by CaptainAnderz";
             this.Menu.TransparencyKey = System.Drawing.Color.Empty;
             this.Menu.Transparent = false;
+            // 
+            // nsGroupBox1
+            // 
+            this.nsGroupBox1.Controls.Add(this.nsLabel2);
+            this.nsGroupBox1.Controls.Add(this.TelePosition);
+            this.nsGroupBox1.Controls.Add(this.nsLabel1);
+            this.nsGroupBox1.Controls.Add(this.SavePosition);
+            this.nsGroupBox1.DrawSeperator = true;
+            this.nsGroupBox1.Location = new System.Drawing.Point(16, 59);
+            this.nsGroupBox1.Name = "nsGroupBox1";
+            this.nsGroupBox1.Size = new System.Drawing.Size(112, 133);
+            this.nsGroupBox1.SubTitle = "Set Hotkeys";
+            this.nsGroupBox1.TabIndex = 4;
+            this.nsGroupBox1.Text = "nsGroupBox1";
+            this.nsGroupBox1.Title = "Settings";
+            // 
+            // nsLabel2
+            // 
+            this.nsLabel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nsLabel2.Location = new System.Drawing.Point(5, 85);
+            this.nsLabel2.Name = "nsLabel2";
+            this.nsLabel2.Size = new System.Drawing.Size(84, 18);
+            this.nsLabel2.TabIndex = 3;
+            this.nsLabel2.Text = "nsLabel2";
+            this.nsLabel2.Value1 = "Tele Position";
+            this.nsLabel2.Value2 = "";
+            // 
+            // TelePosition
+            // 
+            this.TelePosition.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TelePosition.Location = new System.Drawing.Point(3, 102);
+            this.TelePosition.MaxLength = 32767;
+            this.TelePosition.Multiline = false;
+            this.TelePosition.Name = "TelePosition";
+            this.TelePosition.ReadOnly = false;
+            this.TelePosition.Size = new System.Drawing.Size(106, 22);
+            this.TelePosition.TabIndex = 2;
+            this.TelePosition.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.TelePosition.UseSystemPasswordChar = false;
+            this.TelePosition.Enter += new System.EventHandler(this.TelePosition_Enter);
+            this.TelePosition.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TelePosition_KeyDown);
+            this.TelePosition.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TelePosition_KeyUp);
+            this.TelePosition.Leave += new System.EventHandler(this.TelePosition_Leave);
+            // 
+            // nsLabel1
+            // 
+            this.nsLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nsLabel1.Location = new System.Drawing.Point(5, 41);
+            this.nsLabel1.Name = "nsLabel1";
+            this.nsLabel1.Size = new System.Drawing.Size(84, 18);
+            this.nsLabel1.TabIndex = 1;
+            this.nsLabel1.Text = "nsLabel1";
+            this.nsLabel1.Value1 = "Save Position";
+            this.nsLabel1.Value2 = "";
+            // 
+            // SavePosition
+            // 
+            this.SavePosition.Cursor = System.Windows.Forms.Cursors.Default;
+            this.SavePosition.Location = new System.Drawing.Point(3, 58);
+            this.SavePosition.MaxLength = 32767;
+            this.SavePosition.Multiline = false;
+            this.SavePosition.Name = "SavePosition";
+            this.SavePosition.ReadOnly = false;
+            this.SavePosition.Size = new System.Drawing.Size(106, 22);
+            this.SavePosition.TabIndex = 0;
+            this.SavePosition.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.SavePosition.UseSystemPasswordChar = false;
+            this.SavePosition.Enter += new System.EventHandler(this.SavePosition_Enter);
+            this.SavePosition.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SavePosition_KeyDown);
+            this.SavePosition.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SavePosition_KeyUp);
+            this.SavePosition.Leave += new System.EventHandler(this.SavePosition_Leave);
+            // 
+            // GroupBox
+            // 
+            this.GroupBox.Controls.Add(this.TeleportButton);
+            this.GroupBox.Controls.Add(this.SaveButton);
+            this.GroupBox.DrawSeperator = true;
+            this.GroupBox.Location = new System.Drawing.Point(146, 59);
+            this.GroupBox.Name = "GroupBox";
+            this.GroupBox.Size = new System.Drawing.Size(126, 133);
+            this.GroupBox.SubTitle = " ";
+            this.GroupBox.TabIndex = 3;
+            this.GroupBox.Text = "TeleporterButtons";
+            this.GroupBox.Title = "Teleport";
+            // 
+            // TeleportButton
+            // 
+            this.TeleportButton.Location = new System.Drawing.Point(3, 96);
+            this.TeleportButton.Name = "TeleportButton";
+            this.TeleportButton.Size = new System.Drawing.Size(119, 35);
+            this.TeleportButton.TabIndex = 1;
+            this.TeleportButton.Text = "Teleport";
+            this.TeleportButton.Click += new System.EventHandler(this.TeleportButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(3, 51);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SaveButton.Size = new System.Drawing.Size(119, 35);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
             // nsControlButton1
             // 
@@ -118,6 +233,8 @@
             this.Text = "Form1";
             this.Menu.ResumeLayout(false);
             this.Menu.PerformLayout();
+            this.nsGroupBox1.ResumeLayout(false);
+            this.GroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -129,6 +246,14 @@
         private System.Windows.Forms.Label GameName;
         private NSControlButton nsControlButton1;
         private System.Windows.Forms.Timer CheckGameStatus;
+        private NSGroupBox nsGroupBox1;
+        private NSGroupBox GroupBox;
+        private NSTextBox SavePosition;
+        private NSLabel nsLabel1;
+        private NSLabel nsLabel2;
+        private NSTextBox TelePosition;
+        private NSButton TeleportButton;
+        private NSButton SaveButton;
 
     }
 }
--- Mirrors-Edge-Catalyst-TP/Form1.cs
+++ Mirrors-Edge-Catalyst-TP/Form1.cs
@@ -1,21 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Mirrors_Edge_Catalyst_SpeedOMeter;
 
 namespace Mirrors_Edge_Catalyst_TP
 {
     public partial class Form1 : Form
     {
+        private bool KeyisSet { get; set; }
+        private long SavePosAddressY { get; set; }
+        private long SavePosAddressX { get; set; }
+        private long SavePosAddressZ { get; set; }
+        private float SavePosAddressYFloat { get; set; }
+        private float SavePosAddressXFloat { get; set; }
+        private float SavePosAddressZFloat { get; set; }
+        private MemoryManager Mem;
+        private bool GameProcessIsOpen;
+
         public Form1()
         {
             InitializeComponent();
+            KeyPreview = true;
+            Mem = new MemoryManager();
+            if (SavePosition.Focused)
+            {
+                KeyDown += SavePosition_KeyDown;
+                KeyUp += SavePosition_KeyUp;
+            }
+            if (TelePosition.Focused)
+            {
+                KeyDown += SavePosition_KeyDown;
+                KeyUp += SavePosition_KeyUp;
+            }
         }
 
         private void nsControlButton1_Click(object sender, EventArgs e)
@@ -29,11 +46,217 @@ namespace Mirrors_Edge_Catalyst_TP
             {
                 GameStatus.Text = "RUNNING";
                 GameStatus.ForeColor = Color.Green;
+
+                if (!GameProcessIsOpen)
+                {
+                    Mem.openProcess("MirrorsEdgeCatalyst");
+                    GameProcessIsOpen = true;
+                }
             }
             else
             {
                 GameStatus.Text = "NOT RUNNING";
                 GameStatus.ForeColor = Color.Red;
+            }
+        }
+
+
+        // HotKey setup
+        #region 
+        private void SavePosition_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true; //Suppress the key from being processed by the underlying control.
+            SavePosition.Text = string.Empty; //Empty the content of the textbox
+            KeyisSet = false; //At this point the user has not specified a shortcut.
+
+            //Set the backspace button to specify that the user does not want to use a shortcut.
+            if (e.KeyData == Keys.Back)
+            {
+                SavePosition.Text = Keys.None.ToString();
+                return;
+            }
+
+            //A modifier is present. Process each modifier.
+            //Modifiers are separated by a ",". So we'll split them and write each one to the textbox.
+            foreach (var modifier in e.Modifiers.ToString().Split(','))
+            {
+                if (modifier == "None")
+                {
+                    SavePosition.Text += "";
+                }
+                else
+                {
+                    SavePosition.Text += modifier + " + ";
+                }
+            }
+
+            //KEYCODE contains the last key pressed by the user.
+            //If KEYCODE contains a modifier, then the user has not entered a shortcut. hence, KeyisSet is false
+            //But if not, KeyisSet is true.
+            if (e.KeyCode == Keys.ShiftKey | e.KeyCode == Keys.ControlKey | e.KeyCode == Keys.Menu)
+            {
+                KeyisSet = false;
+            }
+            else
+            {
+                SavePosition.Text += e.KeyCode.ToString();
+                KeyisSet = true;
+            }
+        }
+
+        private void SavePosition_KeyUp(object sender, KeyEventArgs e)
+        {
+            //On KeyUp if KeyisSet is False then clear the textbox.
+            if (KeyisSet == false)
+            {
+                SavePosition.Text = Keys.None.ToString();
+            }
+
+            try
+            {
+                if (SavePosition.Text != Keys.None.ToString() && !SavePosition.Text.Trim().EndsWith("+"))
+                    if (!string.IsNullOrEmpty("SavePosition") && HotKeyShared.IsValidHotkeyName(SavePosition.Text))
+                    {
+                        // Add hotkey
+                    }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to add Hotkey");
+            }
+        }
+
+        private void SavePosition_Enter(object sender, EventArgs e)
+        {
+            KeyDown += SavePosition_KeyDown;
+            KeyUp += SavePosition_KeyUp;
+        }
+
+        private void SavePosition_Leave(object sender, EventArgs e)
+        {
+            KeyDown -= SavePosition_KeyDown;
+            KeyUp -= SavePosition_KeyUp;
+        }
+
+        private void TelePosition_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true; //Suppress the key from being processed by the underlying control.
+            TelePosition.Text = string.Empty; //Empty the content of the textbox
+            KeyisSet = false; //At this point the user has not specified a shortcut.
+
+            //Set the backspace button to specify that the user does not want to use a shortcut.
+            if (e.KeyData == Keys.Back)
+            {
+                TelePosition.Text = Keys.None.ToString();
+                return;
+            }
+
+            //A modifier is present. Process each modifier.
+            //Modifiers are separated by a ",". So we'll split them and write each one to the textbox.
+            foreach (var modifier in e.Modifiers.ToString().Split(','))
+            {
+                if (modifier == "None")
+                {
+                    TelePosition.Text += "";
+                }
+                else
+                {
+                    TelePosition.Text += modifier + " + ";
+                }
+            }
+
+            //KEYCODE contains the last key pressed by the user.
+            //If KEYCODE contains a modifier, then the user has not entered a shortcut. hence, KeyisSet is false
+            //But if not, KeyisSet is true.
+            if (e.KeyCode == Keys.ShiftKey | e.KeyCode == Keys.ControlKey | e.KeyCode == Keys.Menu)
+            {
+                KeyisSet = false;
+            }
+            else
+            {
+                TelePosition.Text += e.KeyCode.ToString();
+                KeyisSet = true;
+            }
+        }
+
+        private void TelePosition_KeyUp(object sender, KeyEventArgs e)
+        {
+            //On KeyUp if KeyisSet is False then clear the textbox.
+            if (KeyisSet == false)
+            {
+                TelePosition.Text = Keys.None.ToString();
+            }
+
+            try
+            {
+                if (TelePosition.Text != Keys.None.ToString() && !TelePosition.Text.Trim().EndsWith("+"))
+                    if (!string.IsNullOrEmpty("SavePosition") && HotKeyShared.IsValidHotkeyName(TelePosition.Text))
+                    {
+                        // Add hotkey
+                    }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to add Hotkey");
+            }
+        }
+
+        private void TelePosition_Enter(object sender, EventArgs e)
+        {
+            KeyDown += TelePosition_KeyDown;
+            KeyUp += TelePosition_KeyUp;
+        }
+
+        private void TelePosition_Leave(object sender, EventArgs e)
+        {
+            KeyDown -= TelePosition_KeyDown;
+            KeyUp -= TelePosition_KeyUp;
+        }
+        #endregion
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            // If game is running
+            if (GameProcessIsOpen)
+            {
+                try
+                {
+                    // 142578A68, 70, 98, 238, 18, 22d4 Y
+                    SavePosAddressY = Mem.ReadInt(Mem.GetModuleAddress("MirrorsEdgeCatalyst.exe") + 0x02578A68);
+                    SavePosAddressY = Mem.ReadInt(SavePosAddressY + 0x70);
+                    SavePosAddressY = Mem.ReadInt(SavePosAddressY + 0x98);
+                    SavePosAddressY = Mem.ReadInt(SavePosAddressY + 0x238);
+                    SavePosAddressY = Mem.ReadInt(SavePosAddressY + 0x18);
+
+                    var quickReadAddress = SavePosAddressY;
+
+                    SavePosAddressY += 0x22d4;
+                    SavePosAddressX = (quickReadAddress + 0x22d0);
+                    SavePosAddressZ = (quickReadAddress + 0x22d8);
+
+                    // Read Float values
+                    SavePosAddressYFloat = (float)Mem.ReadFloat(SavePosAddressY);
+                    SavePosAddressXFloat = (float)Mem.ReadFloat(SavePosAddressX);
+                    SavePosAddressZFloat = (float)Mem.ReadFloat(SavePosAddressZ);
+
+                    Invalidate();
+                }
+                catch (Exception)
+                {
+                    //ignore
+                }
+                
+            }
+        }
+
+        private void TeleportButton_Click(object sender, EventArgs e)
+        {
+            if (GameProcessIsOpen && SavePosAddressY != 0 && SavePosAddressX != 0 && SavePosAddressZ != 0)
+            {
+                Mem.WriteFloat(SavePosAddressY, SavePosAddressYFloat);
+                Mem.WriteFloat(SavePosAddressX, SavePosAddressXFloat);
+                Mem.WriteFloat(SavePosAddressZ, SavePosAddressZFloat);
+                Invalidate();
             }
         }
     }
